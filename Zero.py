33d186bb3d5f6adf66450ff006f3780794332c31
melang lama <melanglama00@gmail.com>
2026-06-09 18:18:06 +0545

    concept of list in py

--- Zero.py
+++ Zero.py
@@ -18,3 +18,29 @@ print(str4)
 print(str4[2:13])
 print(str4[3:4])
 print(str4[6:10])
+
+#calculate the greatest among 3 numders asking the numders from users
+print("Enter the numbers accordingly")
+a= int(input("Enter a num: "))
+b= int(input("Enter b num: "))
+c= int(input("Enter c num: "))
+
+#using the if conditional statement
+if(b<a>c):  
+    print("the gratest number is a: ",a)
+elif(a<b>c): 
+   print("the gratest number is b: ",b)
+elif(a<c>b): 
+   print("the gratest number is c: ",c)
+else: 
+    print("in valid numbers")
+
+#the concept of list in python 
+info= ["milan", 20, "nepal", "happy"]
+print(info)
+
+
+   
+
+
+
