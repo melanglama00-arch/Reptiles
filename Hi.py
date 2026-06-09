38cf3e2144cdd1fd334c20b2bdc22e1a29e55a2a
melang lama <melanglama00@gmail.com>
2026-06-09 15:38:32 +0545

    new file created and differen operators were added

---
+++ Hi.py
@@ -0,0 +1,15 @@
+print("welcome to my second file of python learing")
+#practising the different operatore
+
+#logical operations
+num1=10
+num2=30
+print(num1==num2)
+print(num1<num2)
+
+#arithmetic opertors
+a=2
+b=3
+print(a+b)
+print(a**b)
+print(a*b)
