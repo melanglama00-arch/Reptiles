6dd9ffce226143f39623cc1f6491dee987c939ce
melang lama <melanglama00@gmail.com>
2026-06-09 17:16:17 +0545

    creating new file for string practise

--- Hi.py
+++ Hi.py
@@ -13,3 +13,36 @@ b=3
 print(a+b)
 print(a**b)
 print(a*b)
+
+#type conversion
+c=5
+d=4.5
+sum=a+b #here the d is superior than c so c is converted to d automatically
+print(type(sum))
+print(sum)
+
+#using the concept of type casting
+let="89"
+print(type(let))
+let=int(let)
+num=34
+add=let+num
+print(add)
+
+#the input taking methid in python
+
+print("coading is fun")
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+price=int(input("enter the price: Rs "))
+
+print("Name: ",name)
+print("Age: ",age)
+print("Price: ",price)
+
+#practise queation 
+print("to input and to multiply the numbers")
+num3=int(input("Enter a 1st number: "))
+num4=int(input("Enter a 2nd number: "))
+Mul=num3 * num4
+print("the multiplication of two numbers is= ",Mul)
---
+++ Zero.py
@@ -0,0 +1,20 @@
+print("learning about the strings. \n also string features")
+str3="apnecollege"
+print(len(str3))
+
+
+#string concatination
+str0="Milan"
+str1="juk"
+str2="Lama"
+finalstr=str0 + str1 + str2
+print(finalstr)
+print(len(finalstr))
+
+#slicing in python
+print("the example of slicing of string in python")
+str4="welcometojungle"
+print(str4)
+print(str4[2:13])
+print(str4[3:4])
+print(str4[6:10])
