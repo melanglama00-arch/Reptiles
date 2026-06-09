276e96665b0b6536bc609bcb182fb978dea8c873
melang lama <melanglama00@gmail.com>
2026-06-09 11:40:20 +0545

    learned to take the input from users in py

--- Hello.py
+++ Hello.py
@@ -1,9 +1,17 @@
 print("hello nepal")
 print("hi this is milanjuk lama ")
 print("i am learning the python and exploring")
+#practising the variables in pythine
+print("  ")
+print("Variables in python")
 name="milanjuk"
 sum=1+4
 age=20
-print(sum)
-print(age)
-print(name)
+print("sum of the number is: ",sum)
+print("my age is: ",age)
+print("My name is:",name)
+#taking the input from the users
+
+name=input("name: ")
+age=int(input("number: "))
+price=float(input("price of py: "))
